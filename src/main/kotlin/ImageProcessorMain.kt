d03f9c0f9781ddf5399470e5fa04ddd77e66f946
Humberd <humberd.dev@gmail.com>
2022-11-28 00:15:53 +0100

    Add display image

--- src/main/kotlin/ImageProcessorMain.kt
+++ src/main/kotlin/ImageProcessorMain.kt
@@ -1,7 +1,10 @@
 import db.DbConnector
 import db.models.CaptchaChallengeDb
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
 import org.bytedeco.javacpp.Loader
+import org.bytedeco.javacv.CanvasFrame
+import org.bytedeco.javacv.OpenCVFrameConverter.ToMat
 import org.bytedeco.opencv.opencv_java
 import org.ktorm.dsl.from
 import org.ktorm.dsl.limit
@@ -13,16 +16,25 @@ import java.util.*
 
 private val logger = KotlinLogging.logger {}
 
-fun main() {
+suspend fun main() {
     // https://github.com/bytedeco/javacpp-presets/tree/master/opencv#documentation
     Loader.load(opencv_java::class.java)
+    val canvasFrame = CanvasFrame("Hello")
+    val converter = ToMat()
 
     for (queryRowSet in DbConnector.database.from(CaptchaChallengeDb).select().limit(1)) {
         val imgBase64 = queryRowSet[CaptchaChallengeDb.imageBase64Src]!!
 //        logger.info { imgBase64 }
         val imgBytes = base64ToBytes(imgBase64)
-        val image = Imgcodecs.imdecode(MatOfByte(*imgBytes), Imgcodecs.IMREAD_UNCHANGED)
+        val image = Imgcodecs.imdecode(MatOfByte(*imgBytes), Imgcodecs.IMREAD_COLOR)
+        canvasFrame.showImage(converter.convert(image))
     }
+
+    while(canvasFrame.isVisible) {
+        delay(1)
+    }
+
+    canvasFrame.dispose()
 }
 
 fun base64ToBytes(data: String): ByteArray {
