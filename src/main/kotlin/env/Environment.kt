7d3b93e595746fa6613bba8c36ed0aa16e9467ba
Humberd <humberd.dev@gmail.com>
2022-11-27 20:12:54 +0100

    Add postgres compose script

--- src/main/kotlin/env/Environment.kt
+++ src/main/kotlin/env/Environment.kt
@@ -7,4 +7,6 @@ private val env = dotenv()
 object Environment {
     val CAPTCHA_REQUEST_PAYLOAD = env["CAPTCHA_REQUEST_PAYLOAD"]!!
     val CAPTCHA_REQUEST_COOKIES_HEADER = env["CAPTCHA_REQUEST_COOKIES_HEADER"]!!
+    val POSTGRES_USER = env["POSTGRES_USER"]!!
+    val POSTGRES_PASSWORD = env["POSTGRES_PASSWORD"]!!
 }
